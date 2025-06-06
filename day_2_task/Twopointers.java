fb367b68a42c5e0e59de2e1dc8f0a0b14c0bb555
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-06 11:55:06 +0530

    commit

---
+++ day_2_task/Twopointers.java
@@ -0,0 +1,33 @@
+package addon_dsa;
+
+import java.util.Arrays;
+
+public class Twopointers {
+	
+	    public static void main(String[] args) {
+	        int[] arr = {4, 2, 10, 7, 5, 3, 8};
+	        
+	        
+	        int start = 0;
+	        int end = arr.length - 1;
+	       Arrays.sort(arr);
+	       
+  while (start < end) {
+	            
+	            int temp = arr[start];
+	            arr[start] = arr[end];
+	            arr[end] = temp;
+	            
+	            
+	            start++;
+	            end--;
+	        }
+  System.out.print("Reversed array: ");
+  for (int num : arr) {
+      System.out.print(num + " ");
+  }
+}
+}
+
+	       
+	       
